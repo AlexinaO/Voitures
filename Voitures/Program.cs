b8faef99cbe2f45b5cf5f06a717da48c69aa22b6
YBAZAN <40167348+YBAZAN@users.noreply.github.com>
2018-08-27 12:09:47 +0200

    Liste des modèles Renault (Id = 2)

--- Voitures/Program.cs
+++ Voitures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace Voitures
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Modèles chez Renault");
+
+            var chaineConnexion = "Server=.;Database=Voitures;Trusted_Connection=True;";
+            var connexion = new SqlConnection(chaineConnexion);
+            connexion.Open();
+
+            var commande = connexion.CreateCommand();
+            commande.CommandText = "SELECT Nom FROM Modeles WHERE IdMarque = 2";
+            var dataReader = commande.ExecuteReader();
+            while (dataReader.Read())
+            {
+                var colonneNom = dataReader.GetOrdinal("Nom");
+                Console.WriteLine(dataReader.GetString(colonneNom));
+            }
+
+            connexion.Close();
+
+            Console.ReadKey();
         }
     }
 }
